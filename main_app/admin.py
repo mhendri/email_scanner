0470ea4830cd24e5e93c4c4d44fa41f158427247
Daniel Obi <danobi06@gmail.com>
2016-10-21 02:26:01 -0400

    simple admin interface

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin 
+  
+from .models import Priviledged
+admin.site.register(Priviledged)
 
-# Register your models here.
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Priviledged(models.Model):
+    firstName = models.CharField(max_length = 200)
+    lastName = models.CharField(max_length = 200)
+    email = models.EmailField()
